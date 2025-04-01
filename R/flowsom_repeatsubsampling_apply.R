a196cb0481e43ea88ffaa2f0fdc7348d7365ede9
gunthergl_r2 <gunthergl@gmx.net>
2025-04-01 10:06:34 +0200

    documentation

--- R/flowsom_repeatsubsampling_apply.R
+++ R/flowsom_repeatsubsampling_apply.R
@@ -1,8 +1,30 @@
-# Wrapper to
-# 1) Load processed data
-# 1.2) Subsample the data (multiple times) to the given number of cells
-# 2) load the phenodata
-# 3) FlowSOM on the training data
+#' Apply FlowSOM prediction on repeated subsamplings
+#'
+#' This function performs repeated subsampling of a list of flowFrames, applies a trained FlowSOM model
+#' to the subsampled data, and returns predicted cluster results with normalized cluster proportions.
+#'
+#' @param ff_list A list of `flowFrame` objects to be subsampled and clustered.
+#' @param outdir A character string indicating the output directory where results will be saved.
+#'   If `outdir` exists, the results will be written using `flowsom_wrapper_saving()`.
+#' @param flowsom_result A trained FlowSOM object (typically returned by `flowsom_repeatsubsampling()` which called `cytobench::flowSOM_optimal()`).
+#' @param n_metacluster Integer. Number of metaclusters to use when predicting clusters.
+#'   If NULL, the number of metaclusters in `flowsom_result` will be used.
+#' @param n_subsampling Integer. Number of repeated subsampling iterations to perform. Default is 1.
+#' @param n_subsampled_cells Integer. Number of cells per subsample. If the original sample is smaller,
+#'   upsampling is performed; otherwise, downsampling. If you want to use the original sample size,
+#'  set `n_subsampled_cells` to `Inf`. See `cytobench::subsample_ff()` for details.
+#' @param subsampling_seed_first Integer. Base seed for reproducible subsampling. Each subsample iteration
+#'   uses `subsampling_seed_first + i - 1`.
+#' @param remove_results_keywords Character vector. Names of result slots in `flowsom_result` to be removed from output.
+#'   This is useful to avoid returning large objects that are not needed for further analysis.
+#' @param ... Not used here, but potentially relevant if the calling function is used in a
+#' pipeline with additional parameters.
+#'
+#' @return A nested list of FlowSOM prediction results. For each data slot, proportions of clusters per sample
+#'   are returned (after normalization). Slots defined in `remove_results_keywords` are removed. Each list entry
+#'   contains subsampling-specific results.
+#'
+#' @export
 flowsom_repeatsubsampling_apply <- function(
     ff_list,
     outdir,
@@ -71,8 +93,8 @@ flowsom_repeatsubsampling_apply <- function(
         lapply(x, function(y) {
             if ("sample" %in% colnames(y)) {
                 y <- dplyr::mutate(
-                    y, 
-                    File = sub("_subsampled[0-9]+$", "", sample), 
+                    y,
+                    File = sub("_subsampled[0-9]+$", "", sample),
                     .before = "sample"
                 )
             }
