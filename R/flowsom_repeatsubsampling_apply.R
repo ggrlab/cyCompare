fa77f3b51b10d5b854aa4adc73cc16a176660bb3
gunthergl_r2 <gunthergl@gmx.net>
2025-04-01 10:07:48 +0200

    comments added

--- R/flowsom_repeatsubsampling_apply.R
+++ R/flowsom_repeatsubsampling_apply.R
@@ -30,17 +30,18 @@ flowsom_repeatsubsampling_apply <- function(
     outdir,
     flowsom_result,
     n_metacluster = NULL,
-    n_subsampling = 1, # Number of subsamplings
-    n_subsampled_cells = 10000, # Number of cells to be subsampled (up or downsampling automatically)
+    n_subsampling = 1, 
+    n_subsampled_cells = 10000,
     subsampling_seed_first = 427764,
     remove_results_keywords = c("flowsom_newdata", "cells_clusters_from_train"),
     ...) {
-    # Subsample the data
+    # Perform multiple subsamplings of the input flowFrames
     subsample_multiple <- unlist(
         lapply(1:n_subsampling, function(i) {
             fs_train_subsampled <- flowCore::fsApply(ff_list, function(x) {
                 cytobench::subsample_ff(x, n_cells = n_subsampled_cells, seed = subsampling_seed_first + i - 1)
             })
+            # Append subsample ID to each sample name
             flowCore::sampleNames(fs_train_subsampled) <- paste0(
                 flowCore::sampleNames(fs_train_subsampled),
                 "_subsampled",
@@ -49,16 +50,22 @@ flowsom_repeatsubsampling_apply <- function(
             return(flowCore::flowSet_to_list(fs_train_subsampled))
         })
     )
+
+    # Combine all subsampled flowFrames into a single flowSet
     fs_multiple <- flowCore::flowSet(subsample_multiple)
+
+    # Apply the trained FlowSOM model to the new subsampled data
     fs_full_predictedFS <- cytobench::flowSOM_predict(
         flowsom_result = flowsom_result,
         flowset = fs_multiple,
         n_metacluster = min(
             n_metacluster,
-            # Maximum number of metaclusters from FlowSOM itself:
+            # Ensure we do not exceed the number of metaclusters used in training
             flowsom_result[["fs_res_train"]][["map"]][["nMetaclusters"]]
         )
     )
+
+    # Normalize cluster counts to proportions for each sample
     fs_full_predictedFS[["proportions_per_x"]] <- lapply(fs_full_predictedFS[["ncells_per_x"]], function(x) {
         cluster_columns <- colnames(x)[grepl("[cC]luster", colnames(x))]
         ncells_per_row <- rowSums(x[, cluster_columns])
@@ -67,21 +74,22 @@ flowsom_repeatsubsampling_apply <- function(
             ncol = length(cluster_columns), nrow = nrow(x),
             byrow = FALSE
         )
-        # column1, column2, column3
-        # ncells_row1, ncells_row1, ncells_row1
-        # ncells_row2, ncells_row2, ncells_row2
-        # ...
+        # Divide each cluster count by total cell count per sample
         x[, cluster_columns] <- x[, cluster_columns] / ncells_per_row_mat
+
+        # Sanity check: proportions should sum to 1 per row
         if (!all(rowSums(x[, cluster_columns]) - 1 < .Machine$double.eps)) {
-            # I know this check is slow, but these matrices are going to be small anyways
-            # Especially irrelevant regarding the previous clustering application.
             stop("Proportions do not sum to 1 per sample.")
         }
         x
     })
+
+    # Remove unnecessary components from result list
     for (kw_x in remove_results_keywords) {
         fs_full_predictedFS[[kw_x]] <- NULL
     }
+
+    # Optionally save results to disk
     if (exists(outdir)) {
         flowsom_wrapper_saving(
             fs_applied = fs_full_predictedFS,
@@ -89,6 +97,8 @@ flowsom_repeatsubsampling_apply <- function(
             modelname = basename(outdir)
         )
     }
+
+    # Clean up sample names: remove the "_subsampledX" suffix and add original filename
     fs_full_predictedFS <- lapply(fs_full_predictedFS, function(x) {
         lapply(x, function(y) {
             if ("sample" %in% colnames(y)) {
