cf277da3711a3e2a3771da50c698f36938e5575a
gunthergl_r1 <gunthergl@gmx.net>
2025-08-06 19:50:39 +0200

    tmp

---
+++ R/subsample_multiple.R
@@ -0,0 +1,43 @@
+#' Perform Repeated Subsampling of Flow Cytometry Data
+#'
+#' Applies repeated subsampling to a list or `flowSet` of flowFrames. Each subsample iteration
+#' uses a reproducible seed and appends a unique `_subsampledX` suffix to the sample names.
+#'
+#' @param ff_list A list or `flowSet` of `flowFrame` objects to be subsampled.
+#' @param n_subsampling Integer. Number of repeated subsampling rounds to perform (default: 1).
+#' @param n_subsampled_cells Integer. Number of cells per subsample. Automatically downsamples or upsamples.
+#' @param subsampling_seed_first Integer. Base seed used for reproducibility across iterations.
+#'
+#' @return A list of subsampled `flowFrame` objects, one per sample per iteration. Sample names are suffixed accordingly.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' subsampled_frames <- subsample_multiple(
+#'     ff_list = my_flowset,
+#'     n_subsampling = 3,
+#'     n_subsampled_cells = 5000,
+#'     subsampling_seed_first = 123
+#' )
+#' }
+subsample_multiple <- function(
+    ff_list,
+    n_subsampling = 1,
+    n_subsampled_cells = 10000,
+    subsampling_seed_first = 427764) {
+    unlist(
+        lapply(1:n_subsampling, function(i) {
+            fs_train_subsampled <- flowCore::fsApply(ff_list, function(x) {
+                cytobench::subsample_ff(x, n_cells = n_subsampled_cells, seed = subsampling_seed_first + i - 1)
+            })
+            # Append subsample ID to each sample name
+            flowCore::sampleNames(fs_train_subsampled) <- paste0(
+                flowCore::sampleNames(fs_train_subsampled),
+                "_subsampled",
+                i
+            )
+            return(flowCore::flowSet_to_list(fs_train_subsampled))
+        })
+    )
+}
